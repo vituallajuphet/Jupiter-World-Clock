12c44322694763fcae413ac65a7edcb2f4240f30
Juphet Vitualla <jvitualla@dnamicro.com>
2024-10-29 08:02:37 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ import TimeContainer from "./components/TimeContainer";
 
 export default function Home() {
   return (
-    <div>
-      <div className="w-full bg-slate-800 ">
+    <div className="">
+      <div className="w-full bg-slate-800">
         <div className="max-w-[1400px] mx-auto">
           <header className="py-4">
             <h1 className='text-xl'>Jupiter World Clock</h1>
           </header>
         </div>
       </div>
-      <div className="w-full max-w-[1400px] mx-auto">
+      <div className="w-full max-w-[1400px] mx-auto h-screen">
         <div className="my-4">
           <p className="text-sm">This site is intended for Forex trading purposes.</p>
         </div>
